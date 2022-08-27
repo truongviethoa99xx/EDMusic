5af1ca5658a028d69280e63b0c25ab54b1234896
truongl1es <tgian4958@gmail.com>
2022-08-27 23:56:23 +0700

    truong commit 2708-12h

---
+++ resources/views/listen_music/index.blade.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="en">
+   @include('layout.head')
+   <link
+       rel="stylesheet"
+       href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"
+   />
+<body>
+<div id="play-music-listen">
+    <div class="play-container row container-fluid">
+        <div class="play-left col-lg-4">
+            <div class="play-left-header">
+                <div id="play-img-rotate">
+                    <img class="img active" src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt="">
+                </div>
+                <div class="media-content">
+                    <h3 class="media-name">Nuoc mat lung trong</h3>
+                    <div id="media-info">
+                        <p>ngoc kara, vuong anh tu - <span>24/06/2022</span></p>
+                        <p>88 nguoi yeu thich</p>
+                    </div>
+                    <div class="media-action">
+                        <button><i class='bx bxs-right-arrow' ></i>tiep tuc phat</button>
+                        <div class="level">
+                            <i class='bx bx-heart'></i>
+                            <i class='bx bx-dots-horizontal-rounded' ></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="play-right col-lg-8">
+            <div class="meadia-play">
+                <div class="media-head">
+                    <div class="media-left">
+                        <p>Bài hát</p>
+                    </div>
+                    <div class="media-right">
+                        <p>thoi gian</p>
+                    </div>
+                </div>
+                <div class="media-head">
+                    <div class="media-left">
+                        <input type="checkbox">
+                        <img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt="">
+                        <div class="media-info">
+                            <p class="title">
+                                Lorem ipsum dolor sit amet, consectetur.
+                            </p>
+                            <p class="singer">Johnny Dang</p>
+                        </div>
+                    </div>
+                    <div class="media-right">
+                        <p>03:00</p>
+                    </div>
+                </div>
+
+                <div class="list-song">
+                    <h4>co the ban quan tam</h4>
+                    <div class="list">
+                        <!-- item -->
+                        <div class="play-item media-head">
+                            <div class="media-left">
+                                <i class='bx bxs-music' ></i>
+                                <img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt="">
+                                <div class="media-info">
+                                    <p class="title">
+                                        Lorem ipsum dolor sit amet, consectetur.
+                                    </p>
+                                    <p class="singer">Johnny Dang</p>
+                                </div>
+                            </div>
+                            <div class="media-ablum">
+                                if you never go alone
+                            </div>
+                            <div class="media-right">
+                                <p>03:00</p>
+                            </div>
+                        </div>
+                        <!-- item -->
+                        <div class="play-item media-head">
+                            <div class="media-left">
+                                <i class='bx bxs-music' ></i>
+                                <img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt="">
+                                <div class="media-info">
+                                    <p class="title">
+                                        Lorem ipsum dolor sit amet, consectetur.
+                                    </p>
+                                    <p class="singer">Johnny Dang</p>
+                                </div>
+                            </div>
+                            <div class="media-ablum">
+                                if you never go alone
+                            </div>
+                            <div class="media-right">
+                                <p>03:00</p>
+                            </div>
+                        </div>
+                        <!-- item -->
+                        <div class="play-item media-head">
+                            <div class="media-left">
+                                <i class='bx bxs-music' ></i>
+                                <img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt="">
+                                <div class="media-info">
+                                    <p class="title">
+                                        Lorem ipsum dolor sit amet, consectetur.
+                                    </p>
+                                    <p class="singer">Johnny Dang</p>
+                                </div>
+                            </div>
+                            <div class="media-ablum">
+                                if you never go alone
+                            </div>
+                            <div class="media-right">
+                                <p>03:00</p>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- nghệ sĩ tham gia -->
+    <div class="compose-container container-fluid">
+        <h3>nghe si tham gia</h3>
+        <div class="swiper compose ">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+
+    <!-- nghệ sĩ tham gia -->
+    <div class="compose-container container-fluid">
+        <h3>nghe si tham gia</h3>
+        <div class="swiper compose ">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+                <div class="swiper-slide"><img src="https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_webp/cover/3/d/f/4/3df4a79c75c3407c9866d16c41cd57d6.jpg" alt=""></div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+
+
+</div>
+</body>
+@include('layout.script')
+   <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+   <script src="js/truong.js"></script>
+</html>
