6337a68a2b1906befb95f490db12ee089ec5a94d
PhanHaiThanhPY <103014735+PhanHaiThanhPY@users.noreply.github.com>
2022-08-29 20:35:14 +0700

    thanh
    giao dien phan login

---
+++ public/js/thanh.js
@@ -0,0 +1,28 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
+
+function remcl(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus");
+    }
+}
+
+
+inputs.forEach(input => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+});
+
+function validate(){
+    if(document.myForm.name.value == ""){
+        alert( "Please provide your name!" );
+        document.myForm.Name.focus() ;
+        return false;
+    }
+}
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,6 +2,66 @@
 <html lang="en">
 @include('layout.head')
 <body>
-    
+    <img class="wave" src="{{ asset('images/logo/wave.png') }}" alt="" >
+    <div class="container">
+        <div class="img">
+            <img src="{{ asset('images/logo/headphone-animate (1).svg') }}" alt="">
+        </div>
+        <div class="login-content">
+            <form action="index.html">
+                <img src="{{ asset('images/logo/avatar.svg') }}" alt="">
+                <h3 class="title">Create your account</h3>
+                <div class="input-div one" name="myForm" onsubmit = "return(validate());">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div" >
+                        <h5>Username</h5>
+                        <input type="text" class="input" name="name">
+                    </div>
+                </div>
+                <div class="input-div one">
+                    <div class="i">
+                        <!-- <i class="fa-solid fa-cake-candles"></i> -->
+                    </div>
+                    <div class="div">
+                        <h5>Ngày sinh</h5>
+                        <input type="text" class="input" name="email">
+                    </div>
+                </div>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-phone"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Phone Number</h5>
+                        <input type="text" class="input" name="phone">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Password</h5>
+                        <input type="password" class="input" name="pass">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Confirm Password</h5>
+                        <input type="password" class="input" name="confirm_pass">
+                    </div>
+                </div>
+                <input type="submit" class="btn" value="Login"/>
+
+            </form>
+        </div>
+    </div>
+<script src="js/thanh.js"></script>
+<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 </body>
 </html>
